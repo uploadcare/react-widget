1d53ded48155b6d6bc404fa113b64aa3c819c084
Zarema <zarema.khalilova@gmail.com>
2018-02-12 19:03:10 +0300

    Add spaces for text in App

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ const App = () => (
   <div>
     <h1>Try Uploadcare</h1>
     <p>
-      <label htmlFor='file'>Your file:</label>
+      <label htmlFor='file'>Your file:</label>{' '}
       <Uploader id='file' name='file' onChange={(value) => console.log('value: ', value)} />
     </p>
     <p>
-      <label htmlFor='images'>Your images:</label>
-      <Uploader id='images' name='file' data-images-only data-multiple data-crop='free' />
+      <label htmlFor='images'>Your images:</label>{' '}
+      <Uploader id='images' name='file' data-images-only data-multiple />
     </p>
   </div>
 )
