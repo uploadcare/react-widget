dfc3505c6f47e0c4ddb5fef8f5612128d5eb2ca0
Zarema <zarema.khalilova@gmail.com>
2018-05-10 16:27:54 +0300

    Add component for fire unmounting of Demo

---
+++ src/components/ToggleMounting.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+
+class ToggleMounting extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {mounted: true}
+    this.handleMountClick = this.handleMountClick.bind(this)
+  }
+
+  handleMountClick() {
+    this.setState({mounted: !this.state.mounted})
+  }
+
+  render() {
+    const {children, title} = this.props
+    const {mounted} = this.state
+
+    return (
+      <React.Fragment>
+        {mounted ? children : null}
+        <hr/>
+        <button type='button' onClick={this.handleMountClick}>
+          {mounted ? 'Unmount' : 'Mount'} {title}
+        </button>
+      </React.Fragment>
+    )
+  }
+}
+
+export default ToggleMounting
