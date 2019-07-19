003ed4dbca3fca5df541295876894bc7a1c75bf4
Ivakhnenko Dmitry <jeetiss@yandex.ru>
2019-07-19 17:32:35 +0300

    translations and locale (#20)

--- src/index.js
+++ src/index.js
@@ -3,18 +3,37 @@ import { lazy, ClientSuspense } from '@uploadcare/client-suspense'
 
 const Uploader = lazy(() => import('./uploader'))
 
-const Config = () => {
+const Config = ({ locale, localeTranslations, localePluralize }) => {
   useLayoutEffect(() => {
     window.UPLOADCARE_LIVE = false
     window.UPLOADCARE_MANUAL_START = true
   }, [])
 
+  // configurate translations
+  useLayoutEffect(() => {
+    window.UPLOADCARE_LOCALE = window.UPLOADCARE_LOCALE || locale
+    window.UPLOADCARE_LOCALE_TRANSLATIONS =
+      window.UPLOADCARE_LOCALE_TRANSLATIONS || localeTranslations
+    window.UPLOADCARE_LOCALE_PLURALIZE =
+      window.UPLOADCARE_LOCALE_PLURALIZE || localePluralize
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return null
 }
 
-export const Widget = props => (
+export const Widget = ({
+  locale,
+  localeTranslations,
+  localePluralize,
+  ...props
+}) => (
   <>
-    <Config />
+    <Config
+      locale={locale}
+      localeTranslations={localeTranslations}
+      localePluralize={localeTranslations}
+    />
 
     <ClientSuspense fallback='loading...'>
       <Uploader {...props} />
