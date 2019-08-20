ce3a0fdd6a9a60222c1c60d7bb5a60a720b9aa20
Ivakhnenko Dmitry <jeetiss@yandex.ru>
2019-08-20 14:01:02 +0300

    Apply suggestions from code review
    Co-Authored-By: Aleksandr Grenishin <nd0ut.me@gmail.com>

--- src/hooks/use-validators.js
+++ src/hooks/use-validators.js
@@ -3,13 +3,13 @@ import { useEffect } from 'react'
 const useValidators = (widget, validators) => {
   useEffect(() => {
     if (validators != null) {
-      const instanse = widget.current
+      const instance = widget.current
       const _validators = new Set(validators)
 
-      instanse.validators = [...instanse.validators, ...validators]
+      instance.validators = [...instance.validators, ...validators]
 
       return () => {
-        instanse.validators = instanse.validators.filter(validator => !_validators.has(validator))
+        instance.validators = instance.validators.filter(validator => !_validators.has(validator))
       }
     }
   }, [widget, validators])
