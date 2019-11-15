99aa82c2affa4f022bfc2f00ef0d478c96e6cd6b
Siarhei Bautrukevich <bautrukevich@users.noreply.github.com>
2019-11-15 11:44:13 +0300

    feat: add type definitions for typescript (#55)

---
+++ types/index.d.ts
@@ -0,0 +1,333 @@
+import { ComponentType, Ref, RefForwardingComponent } from 'react';
+import JQuery = require('jquery');
+
+export enum Locale {
+  en = 'en',
+  ar = 'ar',
+  az = 'az',
+  ca = 'ca',
+  cs = 'cs',
+  da = 'da',
+  de = 'de',
+  el = 'el',
+  es = 'es',
+  et = 'et',
+  fr = 'fr',
+  he = 'he',
+  it = 'it',
+  ja = 'ja',
+  ko = 'ko',
+  lv = 'lv',
+  nb = 'nb',
+  nl = 'nl',
+  pl = 'pl',
+  pt = 'pt',
+  ro = 'ro',
+  ru = 'ru',
+  sk = 'sk',
+  sr = 'sr',
+  sv = 'sv',
+  tr = 'tr',
+  uk = 'uk',
+  vi = 'vi',
+  zhTW = 'zhTW',
+  zh = 'zh',
+}
+
+export type LocalePluralize = (n: number) => string;
+
+export interface LocaleTranslations {
+  uploading?: string;
+  loadingInfo?: string;
+  errors?: {
+    default?: string;
+    baddata?: string;
+    size?: string;
+    upload?: string;
+    user?: string;
+    info?: string;
+    image?: string;
+    createGroup?: string;
+    deleted?: string;
+  };
+  draghere?: string;
+  file?: {
+    one?: string;
+    other?: string;
+  };
+  buttons?: {
+    cancel?: string;
+    remove?: string;
+    choose?: {
+      files?: {
+        one?: string;
+        other?: string;
+      },
+      images?: {
+        one?: string;
+        other?: string;
+      }
+    }
+  };
+  dialog?: {
+    close?: string;
+    openMenu?: string;
+    done?: string;
+    showFiles?: string;
+    tabs?: {
+      names?: {
+        'empty-pubkey'?: string;
+        preview?: string;
+        file?: string;
+        url?: string;
+        camera?: string;
+        facebook?: string;
+        dropbox?: string;
+        gdrive?: string;
+        gphotos?: string;
+        instagram?: string;
+        vk?: string;
+        evernote?: string;
+        box?: string;
+        onedrive?: string;
+        flickr?: string;
+        huddle?: string;
+      };
+      file?: {
+        drag?: string;
+        nodrop?: string;
+        cloudsTip?: string;
+        or?: string;
+        button?: string;
+        also?: string;
+      };
+      url?: {
+        title?: string;
+        line1?: string;
+        line2?: string;
+        input?: string;
+        button?: string;
+      };
+      camera?: {
+        title?: string;
+        capture?: string;
+        mirror?: string;
+        startRecord?: string;
+        stopRecord?: string;
+        cancelRecord?: string;
+        retry?: string;
+        pleaseAllow?: {
+          title?: string;
+          text?: string;
+        };
+        notFound?: {
+          title?: string;
+          text?: string;
+        }
+      };
+      preview?: {
+        unknownName?: string;
+        change?: string;
+        back?: string;
+        done?: string;
+        unknown?: {
+          title?: string;
+          done?: string;
+        };
+        regular?: {
+          title?: string;
+          line1?: string;
+          line2?: string;
+        };
+        image?: {
+          title?: string;
+          change?: string;
+        };
+        crop?: {
+          title?: string;
+          done?: string;
+          free?: string;
+        };
+        video?: {
+          title?: string;
+          change?: string;
+        };
+        error?: {
+          default?: {
+            title?: string;
+            text?: string;
+            back?: string;
+          };
+          image?: {
+            title?: string;
+            text?: string;
+            back?: string;
+          };
+          size?: {
+            title?: string;
+            text?: string;
+          };
+          loadImage?: {
+            title?: string;
+            text?: string;
+          }
+        };
+        multiple?: {
+          title?: string;
+          question?: string;
+          tooManyFiles?: string;
+          tooFewFiles?: string;
+          clear?: string;
+          done?: string;
+          file?: {
+            preview?: string;
+            remove?: string;
+          }
+        }
+      }
+    };
+    footer?: {
+      text?: string;
+      link?: string;
+    }
+  };
+}
+
+export interface GeoLocation {
+  latitude: number;
+  longitude: number;
+}
+
+export interface OriginalImageInfo {
+  height: number;
+  width: number;
+  geo_location: null | GeoLocation;
+  datetime_original: string;
+  format: string;
+  color_mode: string;
+  dpi: null | number[];
+  orientation: null | number;
+  sequence?: boolean;
+}
+
+export type Uuid = string;
+
+export interface SourceInfo {
+  source: string;
+  file: any;
+}
+
+export interface FileInfo {
+  uuid: Uuid;
+  name: null | string;
+  size: null | number;
+  isStored: null | boolean;
+  isImage: null | boolean;
+  originalImageInfo: null | OriginalImageInfo;
+  mimeType: string;
+  originalUrl: null | string;
+  cdnUrl: null | string;
+  cdnUrlModifiers: null | string;
+  sourceInfo: null | SourceInfo;
+}
+
+export type OnTabVisibilityCallback = (tab: string, shown: boolean) => void;
+
+export interface DialogApi {
+  addFiles(files: FileInfo[]): void;
+  switchTab(tab: string): void;
+  fileColl: FileInfo[];
+  hideTab(tab: string): void;
+  showTab(tab: string): void;
+  isTabVisible(tab: string): boolean;
+  onTabVisibility(callback: OnTabVisibilityCallback): void;
+}
+
+export enum Crop {
+  Disabled = 'disabled',
+  Free = 'free',
+  Default = ''
+}
+
+export interface Settings {
+  // developer hooks
+  locale?: Locale;
+  localePluralize?: (n: number) => string;
+  localeTranslations?: LocaleTranslations;
+  // widget & dialog settings
+  systemDialog?: boolean;
+  crop?: Crop | string;
+  previewStep?: boolean;
+  imagesOnly?: boolean;
+  clearable?: boolean;
+  multiple?: boolean;
+  multipleMax?: number;
+  multipleMin?: number;
+  multipleMaxStrict?: boolean;
+  imageShrink?: string;
+  pathValue?: boolean;
+  tabs?: string;
+  preferredTypes?: string;
+  inputAcceptTypes?: string; // '' means default, null means "disable accept"
+  // upload settings
+  doNotStore?: boolean;
+  publicKey: string | null;
+  secureSignature?: string;
+  secureExpire?: number;
+  pusherKey?: string;
+  cdnBase?: string;
+  urlBase?: string;
+  socialBase?: string;
+  previewProxy?: string | null;
+  previewUrlCallback?: ((originalUrl: string, fileInfo: FileInfo) => string) | null;
+  // fine tuning
+  imagePreviewMaxSize?: number;
+  multipartMinSize?: number;
+  multipartPartSize?: number;
+  multipartMinLastPartSize?: number;
+  multipartConcurrency?: number;
+  multipartMaxAttempts?: number;
+  parallelDirectUploads?: number;
+  passWindowOpen?: boolean;
+  // camera recording
+  audioBitsPerSecond?: number | null;
+  videoBitsPerSecond?: number | null;
+  // maintain settings
+  scriptBase?: string;
+  debugUploads?: boolean;
+  integration?: string;
+}
+
+export interface WidgetAPI {
+  openDialog: (tab: string) => void;
+  reloadInfo: () => void;
+  getInput: () => HTMLInputElement;
+}
+
+export interface WidgetProps extends Settings {
+  value?: string;
+  onChange?: (fileInfo: FileInfo) => void;
+  onFileSelect?: (fileInfo: FileInfo) => void;
+  customTabs?: {[key: string]: CustomTabConstructor};
+  validators?: Validator[];
+  preloader?: ComponentType;
+  ref?: Ref<WidgetAPI>;
+}
+
+export type CustomTabConstructor = (
+  container: JQuery,
+  button: JQuery,
+  dialogApi: DialogApi,
+  settings: Settings,
+  name: string,
+  uploadcare: any
+) => void;
+
+export type Validator = ((fileInfo: FileInfo) => void);
+
+export const Widget: RefForwardingComponent<{
+  locale?: Locale;
+  localeTranslations?: LocaleTranslations;
+  localePluralize?: LocalePluralize;
+  preloader?: string;
+}, WidgetProps>;
---
+++ types/test.tsx
@@ -0,0 +1,132 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Widget, CustomTabConstructor, FileInfo, WidgetAPI, Locale, Crop } from '@uploadcare/react-widget';
+
+<Widget publicKey='demopublickey' />;
+
+<Widget
+  publicKey='demopublickey'
+  locale={Locale.de} />;
+
+<Widget
+  publicKey='demopublickey'
+  crop={Crop.Disabled} />;
+
+<Widget
+  publicKey='demopublickey'
+  crop={Crop.Free} />;
+
+<Widget
+  publicKey='demopublickey'
+  crop={Crop.Default} />;
+
+<Widget
+  publicKey='demopublickey'
+  crop='200x300' />;
+
+const fileTypeLimit = (allowedFileTypes: string) => {
+  const types = allowedFileTypes.split(' ');
+
+  return (fileInfo: FileInfo) => {
+    if (fileInfo.name === null) {
+      return;
+    }
+    const extension = fileInfo.name.split('.').pop();
+
+    if (extension && !types.includes(extension)) {
+      throw new Error(`${extension} is not allowed to upload`);
+    }
+  };
+};
+
+const validators = [fileTypeLimit('mp3 avi mp4')];
+
+<Widget
+  publicKey='demopublickey'
+  validators={validators} />;
+
+<Widget
+  publicKey='demopublickey'
+  onChange={console.log}
+  clearable />;
+
+<Widget
+  publicKey='demopublickey'
+  onFileSelect={console.log}
+  clearable
+  multiple />;
+
+const Preloader = () => <div />;
+
+<Widget
+  publicKey='demopublickey'
+  preloader={Preloader}/>;
+
+const widgetApi = React.useRef<WidgetAPI>(null);
+
+<Widget ref={widgetApi} publicKey='demopublickey' />;
+
+const TabButtonIcon = ({ node, children }: { node: Element, children: React.ReactElement }): React.ReactPortal => {
+  return ReactDOM.createPortal(
+    <svg style={{ display: "none" }}>{children}</svg>,
+    node
+  );
+};
+
+const UnsplashCreator: CustomTabConstructor = (
+  container,
+  button,
+  dialog,
+  settings,
+  name,
+  uploadcare
+) => {
+  const buttonNode = button[0];
+
+  buttonNode.title = "Unsplash";
+
+  ReactDOM.render(
+    <>
+      <TabButtonIcon node={buttonNode}>
+        <symbol
+          id={`uploadcare--icon-unsplash`}
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="currentColor"
+            d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"
+          />
+        </symbol>
+      </TabButtonIcon>
+      <div>
+        Random photo from unsplash:
+        <br />
+        <button
+          onClick={() =>
+            dialog.addFiles([
+              uploadcare.fileFrom(
+                "url",
+                "https://source.unsplash.com/random/800x600/",
+                settings
+              )
+            ])
+          }
+        >
+          {"Load ✨"}
+        </button>
+      </div>
+    </>,
+    container[0]
+  );
+};
+
+<Widget
+  previewStep
+  publicKey="demopublickey"
+  tabs="file unsplash"
+  customTabs={{ unsplash: UnsplashCreator }}
+/>;
