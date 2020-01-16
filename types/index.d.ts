bfd969b1ae0bd23b83b13bf6ad5114e585e7b853
nd0ut <nd0ut.me@gmail.com>
2020-01-16 15:54:33 +0300

    allow null type for `preloader` prop

--- types/index.d.ts
+++ types/index.d.ts
@@ -319,7 +319,7 @@ interface WidgetProps extends Settings {
   onFileSelect?: (fileInfo: FileInfo) => void;
   customTabs?: {[key: string]: CustomTabConstructor};
   validators?: Validator[];
-  preloader?: ComponentType;
+  preloader?: ComponentType | null;
   ref?: Ref<WidgetAPI>;
 }
 
