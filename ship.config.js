564c063581b053b065e2bb9fae0a4e77bc4a9613
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2020-02-06 12:35:09 +0300

    chore: update shipjs config (#134)
    * chore: update shipjs config
    * fix typo

--- ship.config.js
+++ ship.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   publishCommand: ({ defaultCommand }) => `${defaultCommand} --access public`,
-  mergeStrategy: { toSameBranch: ['master'] }
+  mergeStrategy: { toSameBranch: ['master'] },
+  slack: {
+    // disable slack notification for `prepared` and `releaseStart` lifecycle.
+    // Ship.js will send slack message only for `releaseSuccess`.
+    prepared: null,
+    releaseStart: null
+  },
+  // skip preparation if master contain only `chore` commits
+  shouldPrepare: ({ releaseType, commitNumbersPerType }) => {
+    const { fix = 0 } = commitNumbersPerType;
+    if (releaseType === "patch" && fix === 0) {
+      return false;
+    }
+    return true;
+  }
 }
