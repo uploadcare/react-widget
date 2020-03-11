85a9590bcf9f8c73c15d7eb8f702443f70c6491c
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2020-03-11 19:06:24 +0300

    chore: update shipjs config (#150)
    * chore: update shipjs config
    * add me as release reviewer

--- ship.config.js
+++ ship.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   publishCommand: ({ defaultCommand }) => `${defaultCommand} --access public`,
   mergeStrategy: { toSameBranch: ['master'] },
+  pullRequestReviewers: ['jeetiss'],
   slack: {
-    // disable slack notification for `prepared` and `releaseStart` lifecycle.
+    // disable slack notification for `prepared` lifecycle.
     // Ship.js will send slack message only for `releaseSuccess`.
     prepared: null,
-    releaseStart: null
   },
   // skip preparation if master contain only `chore` commits
   shouldPrepare: ({ releaseType, commitNumbersPerType }) => {
