2168eb87b1e57e4170a44f06e607d537fa605c6c
jeetiss <jeetiss@yandex.ru>
2020-08-25 18:54:30 +0300

    add test case

--- test/locale.js
+++ test/locale.js
@@ -4,7 +4,9 @@ import React from 'react'
 import {
   render,
   getByText,
-  fireEvent
+  getByTitle,
+  fireEvent,
+  waitFor
 } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
@@ -37,5 +39,57 @@ describe('Uploader', function () {
     )
 
     expect(getByText(container.nextSibling, 'Выберите локальный файл')).toBeInTheDocument()
+  })
+
+  it('should change labels dynamicaly', async function () {
+    const translation = (text) => ({
+      dialog: {
+        tabs: {
+          file: { drag: text }
+        }
+      }
+    })
+
+    const { container, rerender } = render(
+      <Uploader
+        publicKey='demopublickey'
+        localeTranslations={translation('wow!')}
+      />
+    )
+
+    fireEvent(
+      getByText(container, 'Choose a file'),
+      new window.MouseEvent('click', {
+        bubbles: true,
+        cancelable: true
+      })
+    )
+
+    await waitFor(() => expect(getByText(container.nextSibling, 'wow!')).toBeInTheDocument())
+
+    fireEvent(
+      getByTitle(container.nextSibling, 'Close'),
+      new window.MouseEvent('click', {
+        bubbles: true,
+        cancelable: true
+      })
+    )
+
+    rerender(
+      <Uploader
+        publicKey='demopublickey'
+        localeTranslations={translation('dynamic!')}
+      />
+    )
+
+    fireEvent(
+      getByText(container, 'Choose a file'),
+      new window.MouseEvent('click', {
+        bubbles: true,
+        cancelable: true
+      })
+    )
+
+    await waitFor(() => expect(getByText(container.nextSibling, 'dynamic!')).toBeInTheDocument())
   })
 })
