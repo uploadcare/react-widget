c64e038ed9e8f9088645b9da2948324bda5c1498
Henry Myers <hmyers@zola.com>
2021-03-31 09:34:56 -0400

    fix(build): Add minified ESM builds (#252)
    * Add minified esm build
    
    * Update README with available bundles
    
    * Update package lock

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,12 @@ import babel from 'rollup-plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import replace from '@rollup/plugin-replace'
 import replacement from 'rollup-plugin-module-replacement'
+import { terser } from 'rollup-plugin-terser'
 
 import uploadcare from 'uploadcare-widget'
 
 export default [
-  // esm build width all locales
+  // esm build with all locales
   {
     input: 'src/index.js',
     external: [
@@ -32,7 +33,7 @@ export default [
     ]
   },
 
-  // cjs build width all locales
+  // cjs build with all locales
   {
     input: 'src/index.js',
     external: [
@@ -58,7 +59,43 @@ export default [
     ]
   },
 
-  // esm build width en locale (30% smaller)
+  // minified esm build with all locales
+  // Saves ~13 KB on react-widget and ~227 KB on uploadcare-widget
+  {
+    input: 'src/index.js',
+    external: [
+      'react',
+      'react-fast-compare',
+      'uploadcare-widget/uploadcare.min',
+      '@uploadcare/client-suspense'
+    ],
+    output: {
+      format: 'esm',
+      dir: 'min',
+      sourcemap: false
+    },
+    plugins: [
+      replacement({
+        entries: [
+          {
+            find: 'uploadcare-widget',
+            replacement: 'uploadcare-widget/uploadcare.min'
+          }
+        ]
+      }),
+      commonjs({
+        include: /node_modules/,
+        sourceMap: false
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        presets: [['@babel/env', { modules: false }]]
+      }),
+      terser()
+    ]
+  },
+
+  // esm build with en locale only (saves ~147 KB on uploadcare-widget)
   {
     input: 'src/index.js',
     external: [
@@ -89,6 +126,42 @@ export default [
         exclude: 'node_modules/**',
         presets: [['@babel/env', { modules: false }]]
       })
+    ]
+  },
+
+  // minified esm build with en locale only
+  // Saves ~13 KB on react-widget and ~314 KB on uploadcare-widget
+  {
+    input: 'src/index.js',
+    external: [
+      'react',
+      'react-fast-compare',
+      'uploadcare-widget/uploadcare.lang.en.min',
+      '@uploadcare/client-suspense'
+    ],
+    output: {
+      format: 'esm',
+      dir: 'en-min',
+      sourcemap: false
+    },
+    plugins: [
+      replacement({
+        entries: [
+          {
+            find: 'uploadcare-widget',
+            replacement: 'uploadcare-widget/uploadcare.lang.en.min'
+          }
+        ]
+      }),
+      commonjs({
+        include: /node_modules/,
+        sourceMap: false
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        presets: [['@babel/env', { modules: false }]]
+      }),
+      terser()
     ]
   },
 
