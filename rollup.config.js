14a2dab681a5ef3c51332a495be14acb3a57ef01
Aleksandr Grenishin <nd0ut.me@gmail.com>
2023-03-13 14:42:52 +0400

    chore: separate typing files for CJS and ESM modules (#374)

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,14 @@ import commonjs from '@rollup/plugin-commonjs'
 import replace from '@rollup/plugin-replace'
 import replacement from 'rollup-plugin-module-replacement'
 import { terser } from 'rollup-plugin-terser'
+import copy from 'rollup-plugin-copy'
 
 import uploadcare from 'uploadcare-widget'
 
-const bundleConfig = ({format, dir, minify, widgetBundle}) => {
-  const extension = format === 'cjs' ? 'cjs' : 'mjs'
+const bundleConfig = ({ format, dir, minify, widgetBundle }) => {
+  const bundleExtension = format === 'cjs' ? 'cjs' : 'mjs'
+  const typesExtenson = format === 'cjs' ? 'cts' : 'mts'
+
   return {
     input: 'src/index.js',
     external: [
@@ -17,21 +20,22 @@ const bundleConfig = ({format, dir, minify, widgetBundle}) => {
       '@uploadcare/client-suspense'
     ],
     output: {
-      entryFileNames: `[name].${extension}`,
-      chunkFileNames: `[name]-[hash].${extension}`,
+      entryFileNames: `[name].${bundleExtension}`,
+      chunkFileNames: `[name]-[hash].${bundleExtension}`,
       format,
       dir,
       sourcemap: false
     },
     plugins: [
-      widgetBundle && replacement({
-        entries: [
-          {
-            find: 'uploadcare-widget',
-            replacement: `uploadcare-widget/${widgetBundle}`
-          }
-        ]
-      }),
+      widgetBundle &&
+        replacement({
+          entries: [
+            {
+              find: 'uploadcare-widget',
+              replacement: `uploadcare-widget/${widgetBundle}`
+            }
+          ]
+        }),
       commonjs({
         include: /node_modules/,
         sourceMap: false
@@ -40,31 +44,68 @@ const bundleConfig = ({format, dir, minify, widgetBundle}) => {
         exclude: 'node_modules/**',
         presets: [['@babel/env', { modules: false }]]
       }),
-      minify && terser()
+      minify && terser(),
+      copy({
+        targets: [
+          {
+            src: 'types/index.d.ts',
+            dest: dir,
+            rename: `index.d.${typesExtenson}`
+          }
+        ]
+      })
     ].filter(Boolean)
   }
 }
 
 export default [
   // esm build with all locales
-  bundleConfig({format: 'esm', dir: 'dist/esm'}),
+  bundleConfig({ format: 'esm', dir: 'dist/esm' }),
 
   // cjs build with all locales
-  bundleConfig({format: 'cjs', dir: 'dist/cjs'}),
+  bundleConfig({ format: 'cjs', dir: 'dist/cjs' }),
 
   // minified builds with all locales
   // Saves ~13 KB on react-widget and ~227 KB on uploadcare-widget
-  bundleConfig({format: 'cjs', dir: 'dist/cjs-min', widgetBundle: 'uploadcare.min.js', minify: true}),
-  bundleConfig({format: 'esm', dir: 'dist/esm-min', widgetBundle: 'uploadcare.min.js', minify: true}),
+  bundleConfig({
+    format: 'cjs',
+    dir: 'dist/cjs-min',
+    widgetBundle: 'uploadcare.min.js',
+    minify: true
+  }),
+  bundleConfig({
+    format: 'esm',
+    dir: 'dist/esm-min',
+    widgetBundle: 'uploadcare.min.js',
+    minify: true
+  }),
 
   // builds with en locale only (saves ~147 KB on uploadcare-widget)
-  bundleConfig({format: 'esm', dir: 'dist/esm-en', widgetBundle: 'uploadcare.lang.en.js'}),
-  bundleConfig({format: 'cjs', dir: 'dist/cjs-en', widgetBundle: 'uploadcare.lang.en.js'}),
+  bundleConfig({
+    format: 'esm',
+    dir: 'dist/esm-en',
+    widgetBundle: 'uploadcare.lang.en.js'
+  }),
+  bundleConfig({
+    format: 'cjs',
+    dir: 'dist/cjs-en',
+    widgetBundle: 'uploadcare.lang.en.js'
+  }),
 
   // minified builds with en locale only
   // Saves ~13 KB on react-widget and ~314 KB on uploadcare-widget
-  bundleConfig({format: 'esm', dir: 'dist/esm-en-min', widgetBundle: 'uploadcare.lang.en.min.js', minify: true}),
-  bundleConfig({format: 'cjs', dir: 'dist/cjs-en-min', widgetBundle: 'uploadcare.lang.en.min.js', minify: true}),
+  bundleConfig({
+    format: 'esm',
+    dir: 'dist/esm-en-min',
+    widgetBundle: 'uploadcare.lang.en.min.js',
+    minify: true
+  }),
+  bundleConfig({
+    format: 'cjs',
+    dir: 'dist/cjs-en-min',
+    widgetBundle: 'uploadcare.lang.en.min.js',
+    minify: true
+  }),
 
   {
     input: 'src/langs.js',
