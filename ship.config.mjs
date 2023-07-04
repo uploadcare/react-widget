d2cce69b2e8d53a4bbd1bf2a1d9dca45a402b030
Aleksandr Grenishin <nd0ut.me@gmail.com>
2023-07-04 12:16:21 +0300

    chore: disable slack hooks (#388)

--- ship.config.mjs
+++ ship.config.mjs
@@ -1,10 +1,5 @@
 export default {
   publishCommand: ({ defaultCommand }) => `${defaultCommand} --access public`,
   mergeStrategy: { toSameBranch: ['master'] },
-  pullRequestReviewers: ['nd0ut'],
-  slack: {
-    // disable slack notification for `prepared` lifecycle.
-    // Ship.js will send slack message only for `releaseSuccess`.
-    prepared: null
-  }
+  pullRequestReviewers: ['nd0ut']
 }
